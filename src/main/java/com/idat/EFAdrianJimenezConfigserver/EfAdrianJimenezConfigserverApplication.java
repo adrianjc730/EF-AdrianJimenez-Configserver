3ad16b4e72ae9cd559956859d893a38d1e8d7888
adrianjc730 <adrianjc730@gmail.com>
2022-08-18 22:23:03 -0500

    Configserver

---
+++ src/main/java/com/idat/EFAdrianJimenezConfigserver/EfAdrianJimenezConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EFAdrianJimenezConfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAdrianJimenezConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAdrianJimenezConfigserverApplication.class, args);
+	}
+
+}
